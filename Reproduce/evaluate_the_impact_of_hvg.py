f725fc817019655996683bdad39e287d20c3aefb
weizhiting <wztlxmnaix@tongji.edu.cn>
2024-07-04 16:08:34 +0800

    Add files via upload

---
+++ Reproduce/evaluate_the_impact_of_hvg.py
@@ -0,0 +1,71 @@
+from myUtil import *
+import scanpy as sc
+import warnings
+import anndata as ad
+from scipy import sparse
+from scipy import stats
+import signal
+import multiprocessing
+import pickle
+from tqdm import tqdm
+warnings.filterwarnings('ignore')
+
+### #### 
+
+
+def cal_result(dirName):
+    os.chdir(dirName)
+    if not os.path.isfile('mixscape_filter.h5ad'): return
+    adata1 = sc.read_h5ad('mixscape_filter.h5ad')
+    adata1.uns['log1p']["base"] = None
+    sc.pp.highly_variable_genes(adata1, n_top_genes=10000)
+    all_hvgs = adata1[:, adata1.var['highly_variable']].var_names  ### 直接保留的基因
+
+    perts = adata1.obs['gene'].unique()
+    perts = [i for i in perts if i !='CTRL']
+    with open('missingDEG_ratio1.tsv', 'w') as fout:
+        fout.write('Pertb\tMissingNums\ttotalNums\tMissingRatio\n')
+        for pert in tqdm(perts):
+            mylist = []
+            adata3 = adata1[adata1.obs['gene'].isin(['CTRL', pert])]
+            adata3.uns['log1p']["base"] = None
+            sc.pp.highly_variable_genes(adata3, subset=True, n_top_genes=4000)
+            sc.tl.rank_genes_groups(adata3, 'gene', groups=[pert], reference='CTRL')
+            for pvalue, geneName, logfoldchanges in zip(adata3.uns['rank_genes_groups']['pvals_adj'], adata3.uns['rank_genes_groups']['names'],  adata3.uns['rank_genes_groups']['logfoldchanges']):
+                if pvalue[0] <= 0.01 and abs(logfoldchanges[0]) >=1:
+                    mylist.append(geneName[0])
+            tmp = [i for i in mylist if i not in all_hvgs]
+            if len(mylist) !=0:
+                fout.write('{}\t{}\t{}\t{:.3f}\n'.format(pert, len(tmp), len(mylist) ,len(tmp)/ len(mylist)))
+            else:
+                fout.write('{}\t{}\t{}\t{:.3f}\n'.format(pert, len(tmp), 0, 0))
+
+
+### 分析数据
+def analysis1():
+    mylist = []
+    dat1 = pd.read_excel('/home/wzt/project/HC-CrisprScreen/poolSC_data/Sheet10_modif.xlsx')
+    dat1 = dat1[((dat1['QC'] == 'Pass') & (dat1['Modality'] !='ATAC')  & (dat1['Perturbation Type'] == 'Genetic'))]
+    dat1.sort_values('Filter_PerturbationNums', ascending=True, inplace=True)
+    for dirName in tqdm(dat1['Datapath']):
+        os.chdir(dirName)
+        if os.path.isfile('missingDEG_ratio.tsv'):
+            dat = pd.read_csv('missingDEG_ratio.tsv', sep='\t')
+            dat['PertNums']= dat.shape[0]
+            if dat.shape[0] >=10000:continue
+            mylist.append(dat)
+    datAll = pd.concat(mylist)
+
+
+
+
+#### 
+if __name__== '__main__':
+    dat = pd.read_excel('/home/wzt/project/HC-CrisprScreen/poolSC_data/Sheet10_modif.xlsx')
+    dat = dat[((dat['QC'] == 'Pass') & (dat['Modality'] !='ATAC'))]
+    dat.sort_values('Filter_PerturbationNums', ascending=True, inplace=True)
+    for dirName in tqdm(dat['Datapath']):
+        print (dirName)
+        cal_result(dirName)
+
+
---
+++ Reproduce/reProduce_the_result_of_Mixscape.py
@@ -0,0 +1,37 @@
+import scanpy as sc, pandas as pd, anndata as ad
+import warnings, os
+warnings.filterwarnings('ignore')
+from matplotlib import pyplot as plt
+
+pd.set_option('display.float_format', lambda x: '%.4f' % x)
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib
+plt.rcParams.update({'font.size': 20})
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
+plt.rc('font',family='Times New Roman')
+plt.rcParams.update({'font.size': 10})
+
+#dirName = '/home/wzt/project/HC-CrisprScreen/poolSC_data/8ECCITE-seq/PRJNA641353/ECCITE'
+#os.chdir(dirName)
+
+dat = sc.read_h5ad('mixscape_hvg.h5ad')
+
+
+geneList = ['SLC7A11', 'CYTIP', 'CXCL3', 'TNFAIP6', 'ADAMDEC1', 'IL8', 'CDK5RAP2', 'PGD', 'TXN', 'TXNRD1', 'KYNU', 'EREG', 'CXCL2', 'DCSTAMP', 'TMEM255A', 'CCL4', 'SLC12A8',
+           'SOD2', 'SH3BP5', 'SAT1', 'PDE4DIP', 'GCLM', 'FTH1', 'TDP2', 'CCL8', 'GK', 'LRP12', 'IDO1', 'PLA2G7', 'CYP27A1', 'CES1', 'ME1', 'DUSP6', 'C5AR1', 'CCL3', 'CDKN1A',
+           'RIT1', 'TCHH', 'SLAMF7', 'ABCA1', 'MARCKSL1', 'CEBPB', 'LUCAT1', 'EBI3', 'INSIG1', 'PLEK', 'TMEM38B', 'TTLL4', 'IL32', 'COL6A2', 'CXCL9', 'PPP1R15A', 'MSC', 'RAI14', 
+           'PIM1', 'SQSTM1', 'NEAT1', 'C1QA', 'NINJ1', 'CCL13', 'KLF9', 'CD274', 'CAV1', 'SERPINE2']
+geneList = [i for i in geneList if i in dat.var_names]
+
+dat2 = dat[dat.obs['mixscape_class'].isin(['CTRL'])][:100]
+dat3 = dat[dat.obs['mixscape_class'].isin(['CUL3 NP'])][:100]
+dat4 = dat[dat.obs['mixscape_class'].isin(['CUL3 KO'])]
+dat1 =  ad.concat([dat2, dat3, dat4])
+dat1.obs['mixscape_class'] = dat1.obs['mixscape_class'].cat.reorder_categories(['CTRL', 'CUL3 NP', 'CUL3 KO'])
+sc.pp.scale(dat1, max_value=2)
+sc.pl.heatmap(dat1, geneList, groupby='mixscape_class', swap_axes=True, show_gene_labels=True)
+
+    
